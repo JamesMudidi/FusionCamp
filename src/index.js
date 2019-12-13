eefa534bcee61cba050e42a3333d26938f8a3459
James Mudidi <mudidi.jimmy@gmail.com>
2019-12-13 06:16:41 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const App = () => {
   return <div>Hello Welcome to Fusion Api</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  );
