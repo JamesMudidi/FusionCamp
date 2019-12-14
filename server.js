79f87f6ad1ca34e3bf2d7f6619e50c71029d0fd6
James Mudidi <mudidi.jimmy@gmail.com>
2019-12-14 13:52:25 +0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static('dist'));
+
+app.use(express.static('build'));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
